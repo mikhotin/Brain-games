f3decaa81214d9183fcb754b567fba3a3f37f1eb
alexander <ad.mihotin@gmail.com>
2019-09-01 19:00:26 +0300

    fix random

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -5,7 +5,7 @@ console.log('Welcome to the Brain Games!');
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello ${userName}`);
-const getRandomNumber = () => Math.floor(Math.random() * (100 - 1) + 1);
+const getRandomNumber = () => Math.floor(Math.random() * 100);
 const isEven = () => {
   let counter = 1;
   while (counter <= 3) {
