b097509f21c04fdaf3f0e0afeb2503ec59bc4eaf
alexander <ad.mihotin@gmail.com>
2019-09-03 00:12:02 +0300

    calc game was created

--- src/index.js
+++ src/index.js
@@ -32,3 +32,43 @@ export const isEven = () => {
   }
   console.log(`Congratulations, ${userName}!`);
 };
+export const calc = () => {
+  console.log(`Welcome to the Brain Games!
+What is the result of the expression?`);
+  const getRandomNumber = () => Math.floor(Math.random() * 100);
+  const getRandomSymbol = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+  const symbols = '+-*';
+  let counter = 1;
+  while (counter <= 3) {
+    const num1 = getRandomNumber();
+    const num2 = getRandomNumber();
+    const randomSymbol = symbols[getRandomSymbol(0, 2)];
+    const expression = () => {
+      let result = 0;
+      switch (randomSymbol) {
+        case '-':
+          result = num1 - num2;
+          break;
+        case '*':
+          result = num1 * num2;
+          break;
+        default:
+          result = num1 + num2;
+      }
+      return result;
+    };
+    console.log(expression());
+    console.log(`Question: ${num1} ${randomSymbol} ${num2}`);
+    const answer = readlineSync.question('Your answer:');
+    if (Number(answer) === expression()) {
+      console.log('Correct!');
+      counter += 1;
+    } else {
+      console.log(`
+      ${answer} is wrong answer ;(. Correct answer was ${expression()}.
+      Let's try again, ${'userName'}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${'userName'}!`);
+};
